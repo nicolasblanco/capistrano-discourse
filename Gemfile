41b0b825b1b6b03a1eb934d05d819ebecdabed98
slainer68 <slainer68@gmail.com>
2014-02-27 21:29:41 -0800

    Use monit to restart processes, less capistrano code yay

--- Gemfile
+++ Gemfile
@@ -1,8 +1,9 @@
 source 'https://rubygems.org'
 
-ruby '2.1.0'
+ruby '2.1.1'
 
-gem 'capistrano', '~> 3.0.1'
+gem 'capistrano', '~> 3.1.0'
 gem 'capistrano-rails', '~> 1.1'
 gem 'capistrano-rbenv', github: "capistrano/rbenv"
 gem 'pry'
+gem 'capistrano3-monit', github: 'naps62/capistrano3-monit'
--- config/deploy.rb
+++ config/deploy.rb
@@ -21,20 +21,20 @@ set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public
 set :keep_releases, 5
 
 set :rbenv_type, :user
-set :rbenv_ruby, '2.1.0'
+set :rbenv_ruby, '2.1.1'
 
 fetch(:default_env).merge!(ruby_gc_malloc_limit: 90000000)
 set :rbenv_map_bins, %w{rake gem bundle ruby rails thin}
 
 namespace :deploy do
-  desc 'Restart application'
-  task :restart do
-    on roles(:app), in: :sequence, wait: 5 do
-      within release_path do
-        execute :bundle, :exec, :thin, "-C config/thin.yml restart"
-      end
-    end
-  end
+  # desc 'Restart application'
+  # task :restart do
+  #   on roles(:app), in: :sequence, wait: 5 do
+  #     within release_path do
+  #       execute :bundle, :exec, :thin, "-C config/thin.yml stop"
+  #     end
+  #   end
+  # end
 
   #after :restart, :clear_cache do
   #  on roles(:web), in: :groups, limit: 3, wait: 10 do
